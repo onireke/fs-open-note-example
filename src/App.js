8d76adcaf1a0676eadf81cfd5dc9a01e70e75c1e
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-07-24 12:28:24 +0100

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Note from "./components/Note";
 import noteService from "./services/note";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
+import loginService from "./services/login";
 // import axios from "axios";
 
 const App = (props) => {
@@ -10,7 +11,15 @@ const App = (props) => {
   const [newNote, setNewNote] = useState(" ");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState("some error happened...");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
@@ -46,21 +55,73 @@ const App = (props) => {
       setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
-    // axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-    //   setNotes(notes.concat(noteObject));
-    //   setNewNote("");
-    // });
   };
 
-  const handleChage = (event) => {
+  const handleChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("login in with", username, password);
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      window.localStorage.setItem("loggedNoteAppUser", JSON.stringify(user));
+
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleChange} />
+      <button type="submit">save</button>
+    </form>
+  );
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {user === null ? loginForm() : <p>{user.name} logged-in</p>}
 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
@@ -77,11 +138,6 @@ const App = (props) => {
         ))}
       </ul>
 
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleChage} />
-
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </div>
   );
